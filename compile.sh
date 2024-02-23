74c1aa25f6f44672d5e86610e3401b7950157b57
qc903113684 <qc903113684@163.com>
2024-02-23 10:08:58 +0800

    compile.sh

---
+++ compile.sh
@@ -0,0 +1,16 @@
+# llama-2
+MODEL_NAME=Llama-2-7b-chat-hf
+QUANTIZATION=q4f16_0
+CONV_TEMP=llama-2
+CONTEXT_WINDOW_SIZE=768
+
+# convert weights
+# mlc_chat convert_weight /home/chaoqin/model_resource/$MODEL_NAME/ --quantization $QUANTIZATION -o dist/$MODEL_NAME-$QUANTIZATION/
+
+# create mlc-chat-config.json
+# mlc_chat gen_config /home/chaoqin/model_resource/$MODEL_NAME/ --quantization $QUANTIZATION \
+#   --conv-template $CONV_TEMP --context-window-size $CONTEXT_WINDOW_SIZE -o dist/${MODEL_NAME}-${QUANTIZATION}/
+
+# # 2. compile: compile model library with specification in mlc-chat-config.json
+mlc_chat compile ./dist/${MODEL_NAME}-${QUANTIZATION}/mlc-chat-config.json \
+    --device android -o ./dist/${MODEL_NAME}-${QUANTIZATION}/${MODEL_NAME}-${QUANTIZATION}-android.tar
