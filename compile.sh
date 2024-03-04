eeabd9fd78a0f6a8663f507022e32feeb9f8fcff
qc903113684 <qc903113684@163.com>
2024-03-04 13:48:06 +0800

    4j_runtime lib

--- compile.sh
+++ compile.sh
@@ -11,11 +11,11 @@ CONV_TEMP=gemma_instruction
 CONTEXT_WINDOW_SIZE=768
 
 # convert weights
-# mlc_chat convert_weight /home/chaoqin/model_resource/$MODEL_NAME/ --quantization $QUANTIZATION -o dist/$MODEL_NAME-$QUANTIZATION/
+mlc_chat convert_weight /home/chaoqin/model_resource/$MODEL_NAME/ --quantization $QUANTIZATION -o dist/$MODEL_NAME-$QUANTIZATION/
 
 # create mlc-chat-config.json
-# mlc_chat gen_config /home/chaoqin/model_resource/$MODEL_NAME/ --quantization $QUANTIZATION \
-#   --conv-template $CONV_TEMP --context-window-size $CONTEXT_WINDOW_SIZE -o dist/${MODEL_NAME}-${QUANTIZATION}/
+mlc_chat gen_config /home/chaoqin/model_resource/$MODEL_NAME/ --quantization $QUANTIZATION \
+  --conv-template $CONV_TEMP --context-window-size $CONTEXT_WINDOW_SIZE -o dist/${MODEL_NAME}-${QUANTIZATION}/
 
 # # 2. compile: compile model library with specification in mlc-chat-config.json
 mlc_chat compile ./dist/${MODEL_NAME}-${QUANTIZATION}/mlc-chat-config.json \
